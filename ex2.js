bccbc11815eafb64327c0c74815e57446e51bf7e
HibaAd <72765932+HibaAd567@users.noreply.github.com>
2025-11-17 16:21:51 +0100

    hh

--- ex2.js
+++ ex2.js
@@ -8,7 +8,28 @@ export default function Panier () {
     ]);
 
     function ajt() {
+        const newArt = {id: arts.length + 1, nom: "dd", prix: 10, quantite: 3};
+        setArts([...arts, newArt]);
+    };
+    
+    const tot = arts.reduce((sum , {prix, quantite}) => sum + prix * quantite, 0)
 
+    return (
+        <div>
+            <ul>
+                {arts.map(({id, nom, prix ,quantite}) => (
+                    <li key={id}> 
+                        {nom} - prix : {prix * quantite}
+                    </li>
+                ))}
+            </ul>
+
+            <h3> Total : {tot} </h3>
+            <p>{tot > 100 ? "Livraison gratuite !" : "Frais de livraison appliqués"}</p>
+
+            <button onClick={ajt}>Ajouter Article</button>
+            
+        </div>
     )
 }
 
